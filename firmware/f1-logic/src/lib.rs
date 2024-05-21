3baa1142a6c0a4d04cf1962a77a83b608d4e4f07
Adam Hott <24814301+CodingInGreen@users.noreply.github.com>
2024-05-21 10:47:08 +0100

    Fix issue #16 - remove println

--- firmware/f1-logic/src/lib.rs
+++ firmware/f1-logic/src/lib.rs
@@ -1,11 +1,11 @@
 #![no_std]
 #![no_main]
 
+#[main]
 fn main() {
     let a = 5;
     let b = 10;
     let sum = add(a, b);
-    println!("The sum of {} and {} is {}", a, b, sum);
 }
 
 fn add(x: i32, y: i32) -> i32 {
