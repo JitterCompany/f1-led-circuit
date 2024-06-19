c42ed06945b7ab493330021bb093b65f2fc5f557
Adam Hott <24814301+CodingInGreen@users.noreply.github.com>
2024-06-19 14:28:24 +0100

    Update SPI clock rate

--- firmware/f1-hardware/src/main.rs
+++ firmware/f1-hardware/src/main.rs
@@ -66,7 +66,7 @@ async fn main(_spawner: Spawner) {
 
     let (mut descriptors, mut rx_descriptors) = dma_descriptors!(32000);
 
-    let mut spi = Spi::new(peripherals.SPI2, 100.kHz(), SpiMode::Mode0, &clocks)
+    let mut spi = Spi::new(peripherals.SPI2, 20.MHz(), SpiMode::Mode0, &clocks)
         .with_pins(Some(sclk), Some(mosi), Some(miso), Some(cs))
         .with_dma(dma_channel.configure_for_async(
             false,
