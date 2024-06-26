462cde881e14a3957253cfdddf30ab315a83fe37
Adam Hott <24814301+CodingInGreen@users.noreply.github.com>
2024-06-26 10:52:52 +0100

    Fix CONST data example

--- firmware/f1-hardware/src/data.rs
+++ firmware/f1-hardware/src/data.rs
@@ -1,5 +1,3 @@
-use heapless::Vec;
-
 #[derive(Debug)]
 pub struct DriverData {
     pub driver_number: u32,
@@ -8,33 +6,67 @@ pub struct DriverData {
 
 #[derive(Debug)]
 pub struct UpdateFrame {
-    pub drivers: Vec<DriverData, 2048>,
+    pub drivers: [Option<DriverData>; 20]
 }
 
 #[derive(Debug)]
 pub struct VisualizationData {
     pub update_rate_ms: u32,
-    pub frames: Vec<UpdateFrame, 2048>,
+    pub frames: [UpdateFrame; 2],
 }
 
 
 pub const VISUALIZATION_DATA: VisualizationData = VisualizationData {
     update_rate_ms: 1000,
-    frames: {
+    frames: [
         UpdateFrame {
-            drivers: {
-                DriverData { driver_number: 1, led_num: 1 },
-                DriverData { driver_number: 2, led_num: 2 },
-                DriverData { driver_number: 3, led_num: 3 },
-            },
+            drivers: [
+                Some(DriverData { driver_number: 1, led_num: 1 }),
+                Some(DriverData { driver_number: 2, led_num: 2 }),
+                Some(DriverData { driver_number: 3, led_num: 3 }),
+                None,
+                None,
+                None,
+                None,
+                None,
+                None,
+                None,
+                None,
+                None,
+                None,
+                None,
+                None,
+                None,
+                None,
+                None,
+                None,
+                None,
+            ],
         },
         UpdateFrame {
-            drivers: {
-                DriverData { driver_number: 4, led_num: 4 },
-                DriverData { driver_number: 5, led_num: 5 },
-                DriverData { driver_number: 6, led_num: 6 },
-            },
+            drivers: [
+                Some(DriverData { driver_number: 4, led_num: 4 }),
+                Some(DriverData { driver_number: 5, led_num: 5 }),
+                Some(DriverData { driver_number: 6, led_num: 6 }),
+                Some(DriverData { driver_number: 7, led_num: 7 }),
+                None,
+                None,
+                None,
+                None,
+                None,
+                None,
+                None,
+                None,
+                None,
+                None,
+                None,
+                None,
+                None,
+                None,
+                None,
+                None,
+            ],
         },
-    },
+    ],
 };
 
