d558270e3d0ccb390d6afb460df952f9d43de685
Adam Hott <24814301+CodingInGreen@users.noreply.github.com>
2024-08-01 09:51:44 +0100

    Add items from f1-logic

--- firmware/f1-hardware/src/main.rs
+++ firmware/f1-hardware/src/main.rs
@@ -5,7 +5,8 @@
 mod data;
 mod hd108;
 mod driver_info;
-use data::VISUALIZATION_DATA;
+use firmware::f1_logic::data_frame::{DriverData, UpdateFrame, NUM_DRIVERS};
+use firrmware::f1_logic::data_frame::UpdateFrame::{ to_bytes, try_from_bytes };
 use embassy_executor::Spawner;
 use embassy_sync::blocking_mutex::raw::NoopRawMutex;
 use embassy_sync::channel::Channel;
@@ -32,106 +33,6 @@ use heapless::Vec;
 use panic_halt as _;
 use static_cell::StaticCell;
 
-struct RGBColor {
-    r: u8,
-    g: u8,
-    b: u8,
-}
-
-const DRIVER_COLORS: [RGBColor; 20] = [
-    RGBColor {
-        r: 30,
-        g: 65,
-        b: 255,
-    }, // Max Verstappen
-    RGBColor {
-        r: 0,
-        g: 82,
-        b: 255,
-    }, // Logan Sargeant
-    RGBColor {
-        r: 255,
-        g: 135,
-        b: 0,
-    }, // Lando Norris
-    RGBColor {
-        r: 2,
-        g: 144,
-        b: 240,
-    }, // Pierre Gasly
-    RGBColor {
-        r: 30,
-        g: 65,
-        b: 255,
-    }, // Sergio Perez
-    RGBColor {
-        r: 0,
-        g: 110,
-        b: 120,
-    }, // Fernando Alonso
-    RGBColor { r: 220, g: 0, b: 0 }, // Charles Leclerc
-    RGBColor {
-        r: 0,
-        g: 110,
-        b: 120,
-    }, // Lance Stroll
-    RGBColor {
-        r: 160,
-        g: 207,
-        b: 205,
-    }, // Kevin Magnussen
-    RGBColor {
-        r: 60,
-        g: 130,
-        b: 200,
-    }, // Yuki Tsunoda
-    RGBColor {
-        r: 0,
-        g: 82,
-        b: 255,
-    }, // Alex Albon
-    RGBColor {
-        r: 165,
-        g: 160,
-        b: 155,
-    }, // Zhou Guanyu
-    RGBColor {
-        r: 160,
-        g: 207,
-        b: 205,
-    }, // Nico Hulkenberg
-    RGBColor {
-        r: 2,
-        g: 144,
-        b: 240,
-    }, // Esteban Ocon
-    RGBColor {
-        r: 60,
-        g: 130,
-        b: 200,
-    }, // Liam Lawson
-    RGBColor {
-        r: 0,
-        g: 210,
-        b: 190,
-    }, // Lewis Hamilton
-    RGBColor { r: 220, g: 0, b: 0 }, // Carlos Sainz
-    RGBColor {
-        r: 0,
-        g: 210,
-        b: 190,
-    }, // George Russell
-    RGBColor {
-        r: 165,
-        g: 160,
-        b: 155,
-    }, // Valtteri Bottas
-    RGBColor {
-        r: 255,
-        g: 135,
-        b: 0,
-    }, // Oscar Piastri
-];
 
 enum Message {
     ButtonPressed,
@@ -205,6 +106,8 @@ async fn led_task(
     loop {
         // Wait for the start message
         receiver.receive().await;
+
+        /* 
         for i in 0..=96 {
             let color = &DRIVER_COLORS[i % DRIVER_COLORS.len()]; // Get the corresponding color
             hd108.set_led(i, color.r, color.g, color.b).await.unwrap(); // Pass the RGB values directly
@@ -216,6 +119,7 @@ async fn led_task(
             }
             Timer::after(Duration::from_millis(25)).await; // Debounce delay
         }
+        */
     }
 }
 
